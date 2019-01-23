e9ac40bc69d33a89e238384cb75dd286ba256fd5
Daniel Lowman <daniel.lowman@qudini.com>
2019-01-23 14:01:32 +0000

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# react-front-end-pair-programming-exercise
-
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css';
+
+ReactDOM.render(<div>Hello World!</div>, document.getElementById('root'));
